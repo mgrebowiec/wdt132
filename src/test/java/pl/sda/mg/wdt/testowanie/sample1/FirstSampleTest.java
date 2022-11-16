47789fff519c842b3368ae2eaabe1e6a6417ce8e
mgrebowiec <michal.grebowiec@gmail.com>
2022-11-16 19:06:13 +0100

    first test - template

---
+++ src/test/java/pl/sda/mg/wdt/testowanie/sample1/FirstSampleTest.java
@@ -0,0 +1,16 @@
+package pl.sda.mg.wdt.testowanie.sample1;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstSampleTest {
+
+    @Test
+    void shouldReturnSix() {
+        // given
+
+        // when
+
+        // then
+
+    }
+}
