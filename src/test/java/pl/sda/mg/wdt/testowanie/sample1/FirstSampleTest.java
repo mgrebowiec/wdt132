17c39fb6f90ac4a99b3c5efbad79c28db881c8bd
mgrebowiec <michal.grebowiec@gmail.com>
2022-11-16 19:38:42 +0100

    plugin do mavena + pierwszy test

--- src/test/java/pl/sda/mg/wdt/testowanie/sample1/FirstSampleTest.java
+++ src/test/java/pl/sda/mg/wdt/testowanie/sample1/FirstSampleTest.java
@@ -1,5 +1,6 @@
 package pl.sda.mg.wdt.testowanie.sample1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FirstSampleTest {
@@ -7,10 +8,14 @@ public class FirstSampleTest {
     @Test
     void shouldReturnSix() {
         // given
+        int firstArgument = 3;
+        int secondArgument = 3;
 
         // when
+        int actualResult = firstArgument * secondArgument;
 
         // then
+        Assertions.assertEquals(9, actualResult);
 
     }
 }
